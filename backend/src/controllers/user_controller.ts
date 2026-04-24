48ed2a2046f001033302bfd68f15b977b5b8c7c8
evairx <xxbrazzersxx35@gmail.com>
2026-04-23 21:07:57 -0400

    fix

---
+++ backend/src/controllers/user_controller.ts
@@ -0,0 +1,8 @@
+import { Hono } from 'hono';
+import { UserService } from '../services/user_service';
+
+export const userController = new Hono();
+
+userController.post('/signup', async (c) => await UserService.signUp(c) );
+userController.post('/signin', async (c) => await UserService.signIn(c) );
+userController.get('/profile', async (c) => await UserService.profile(c) );
